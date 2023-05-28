04f3d59ed34ec06333d2a382d265cf302e1602ce
Leaguejason <chuunbyou@gmail.com>
2023-05-28 15:41:35 +0700

    be

---
+++ BackEnd/Common/Message.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public static class Message
+    {
+        public static string SUCCESS = "Success";
+        public static string FAILED = "Failed";
+        public static string NOT_FOUND_SCHOLAR = "Not found scholar";
+        public static string NOT_FOUND_COURSE = "Not found course";
+        public static string NOT_FOUND_EVENT = "Not found event";
+        public static string NOT_FOUND_FACUTLY = "Not found facutly";
+        public static string NOT_FOUND_DATA = "Not found data";
+        public static string NOT_FOUND_EXAM = "Not found exam";
+        public static string NOT_FOUND_ACCOUNT = "Not found account";
+        public static string NOT_FOUND_FEEDBACK = "Not found feed back";
+        public static string NOT_FOUND_BATCH = "Not found batch";
+        public static string USERNAME_MATCHING = "The username account has existed!";
+
+        public static string UCOURSE_CODE_ALREADY_EXIST = "Course code already exist";
+        public static string COURSE_HAS_REGISTER = "Course has register";
+
+        public static string INVALID_END_TIME = "End time must be greater than start";
+
+        public static string NOT_FOUNT_FEEDBACK = "Not found feedback.";
+
+    }
+}
